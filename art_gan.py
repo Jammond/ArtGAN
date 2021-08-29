d9e7dac5f61cfcc24b3c56424c7d07cebc895960
Jammond <jammond.hayesruffin@gmail.com>
2021-08-29 07:38:09 +0000

    again

--- art_gan.py
+++ art_gan.py
@@ -1,11 +1,12 @@
 
 import tensorflow as tf
+from tensorflow import keras 
 
-from tf.keras.layers import Input, Reshape, Dropout, Dense, Flatten, BatchNormalization, Activation, ZeroPadding2D
-from tf.keras.layers.advanced_activations import LeakyReLU
-from tf.keras.layers.convolutional import UpSampling2D, Conv2D
-from tf.keras.models import Sequential, Model, load_model
-from tf.keras.optimizers import Adam
+from keras.layers import Input, Reshape, Dropout, Dense, Flatten, BatchNormalization, Activation, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model, load_model
+from keras.optimizers import Adam
 import numpy as np
 from PIL import Image
 import os
